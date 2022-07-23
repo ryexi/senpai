af80cc7c47afc0c5675b41e016059e6980c12a4c
Blank <74569969+imdying@users.noreply.github.com>
2022-07-23 14:18:21 +0400

    Update Cli.cs
    https://github.com/dotnet/csharplang/blob/main/meetings/2022/LDM-2022-04-06.md#conclusion-5

--- src/Cli.cs
+++ src/Cli.cs
@@ -8,14 +8,14 @@ public static class Cli
     /// Initiate the command-line interpreter.
     /// </summary>
     /// <param name="Args">The args of the application.</param>
-    public static int Initialize(string[] Args!!) => Handler.Invoke(Args, null, Assembly.GetCallingAssembly());
+    public static int Initialize(string[] Args) => Handler.Invoke(Args, null, Assembly.GetCallingAssembly());
 
     /// <summary>
     /// Initiate the command-line interpreter.
     /// </summary>
     /// <param name="Args">The args of the application.</param>
     /// <param name="Description">The description of the application.</param>
-    public static int Initialize(string[] Args!!, string? Description) => Handler.Invoke(Args, Description, Assembly.GetCallingAssembly());
+    public static int Initialize(string[] Args, string? Description) => Handler.Invoke(Args, Description, Assembly.GetCallingAssembly());
 
     /// <summary>
     /// Initiate the command-line interpreter.
@@ -23,5 +23,5 @@ public static class Cli
     /// <param name="Args">The args of the application.</param>
     /// <param name="Description">The description of the application.</param>
     /// <param name="Caller">The assembly that contains the decorated methods.</param>
-    public static int Initialize(string[] Args!!, string? Description, Assembly Caller!!) => Handler.Invoke(Args, Description, Caller);
+    public static int Initialize(string[] Args, string? Description, Assembly Caller) => Handler.Invoke(Args, Description, Caller);
 }
