b54ed003499cc3328024a4f7634cc48879826a7f
Blank <74569969+imdying@users.noreply.github.com>
2022-11-05 20:43:44 +0400

    Create Group.cs

---
+++ playground/Sample/Commands/Group.cs
@@ -0,0 +1,18 @@
+﻿namespace Sample.Commands
+{
+    [Command]
+    public static class Group
+    {
+        [Command]
+        public static class Command1 { }
+
+        [Command]
+        public static class Command2 { }
+
+        [Command]
+        public static class Command3 { }
+
+        [Command]
+        public static class Command4 { }
+    }
+}
