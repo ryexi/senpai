46813dd60c5abb8a9299a3e930508bf157339da5
Blank <74569969+imdying@users.noreply.github.com>
2022-12-23 19:01:25 +0400

    Move `Program.Benchmark.cs` into `Program.cs`

--- playground/Test/Program.cs
+++ playground/Test/Program.cs
@@ -1,12 +1,22 @@
 ﻿global using Senpai;
 
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
 namespace Test
 {
     internal partial class Program
     {
+        [ModuleInitializer]
+        public static void Benchmark()
+        {
+            var sw = Stopwatch.StartNew();
+            AppDomain.CurrentDomain.ProcessExit += (s, e) => Console.WriteLine("\nTime elapsed: {0} ms", sw?.ElapsedMilliseconds);
+        }
+
         static void Main(string[] args)
         {
-            Benchmark(() => App.Run(new(args)));
+            App.Run(new(args));
         }
     }
 }
