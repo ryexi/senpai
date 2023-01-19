437c38503f9a43df68772301f8e18206cd9cef5b
Blank <74569969+imdying@users.noreply.github.com>
2023-01-19 18:06:26 +0400

    Update Greetings.cs

--- playground/Sample/Commands/Greetings.cs
+++ playground/Sample/Commands/Greetings.cs
@@ -9,6 +9,12 @@
             set;
         }
 
+        protected override CommandProperty Properties => new()
+        {
+            Name = "greets",
+            Description = "Displays a greeting message followed by a name."
+        };
+
         protected override void Invocation(object?[] args)
         {
             Console.WriteLine("Greetings, {0}.", Name);
