703e0a3759bf98693228e391788e9b17e2053491
Blank <74569969+imdying@users.noreply.github.com>
2023-01-29 17:21:04 +0400

    Add `Facade` and minor changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [1.0.0-preview.1] - 2023-01-29
- 
+ Continuation of [Senpai-3.0.0-preview.1](https://github.com/imdying/maid/tree/senpai-3.0.0-preview.1).
+
+ ## Added
+  - A new class `Facade` that provides an abstraction for the implementation of a command.
+  - A new method `Interrupt()` to `Cli`.
+
  ## Changed
   - Project has been renamed from `Senpai` to `Maid`.
+  - Changed `App` to `Cli`.
+
--- src/Maid/Sdk/Facade.cs
+++ src/Maid/Sdk/Facade.cs
@@ -5,6 +5,48 @@
     /// </summary>
     public abstract class Facade
     {
+        /// <summary>
+        /// Gets or sets a <see cref="bool"/> value that controls the visibility of outputs from <see cref="WriteLine(object?)"/> and similar methods.
+        /// </summary>
+        public virtual bool IsQuiet
+        {
+            get;
+            set;
+        }
 
+        private bool IsVerbose => IsQuiet is false;
+
+        /// <summary>
+        /// If anything of the <see cref="WriteLine(object?)"/> and similar methods have written to the stdout.
+        /// </summary>
+        public bool HasWritten
+        {
+            get;
+            private set;
+        }
+
+        public void WriteLine(object? value)
+        {
+            if (IsVerbose)
+            {
+                if (!HasWritten)
+                    HasWritten = true;
+
+                Console.WriteLine(value);
+            }
+        }
+
+        public void WriteWarning(object? value)
+        {
+            if (IsVerbose)
+            {
+                if (!HasWritten)
+                    HasWritten = true;
+
+                Cli.WriteWarning(value);
+            }
+        }
+
+        public void WriteError(object? value) => Cli.WriteError(value);
     }
 }
